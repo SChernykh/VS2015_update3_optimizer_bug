0271d33bcf2b33979a55dde3ac31303e4fa41862
SChernykh <sergey.v.chernykh@gmail.com>
2016-07-30 16:05:54 +0200

    Reduced code size

--- ConsoleApplication2/ParallelPrimeSieve.cpp
+++ ConsoleApplication2/ParallelPrimeSieve.cpp
@@ -2,24 +2,14 @@
 #include <stdio.h>
 #include <algorithm>
 
-inline uint64_t add_overflow_safe(uint64_t x, uint64_t y)
-{
-	if (x >= std::numeric_limits<uint64_t>::max() - y)
-		return std::numeric_limits<uint64_t>::max();
-
-	return x + y;
-}
-
 uint64_t ParallelPrimeSieve::align(uint64_t n) const
 {
-  if (add_overflow_safe(n, 32) >= stop_)
-    return stop_;
-
-  n = add_overflow_safe(n, 32) - n % 30;
-  return std::min(n, stop_);
+	if (n == start_)
+		return start_;
+	n = std::min(n + 32 - n % 30, stop_);
+	return n;
 }
 
-//#pragma optimize("", off)
 void ParallelPrimeSieve::sieveInternal(uint64_t threadInterval)
 {
 	uint64_t threadStop = start_ + threadInterval;
